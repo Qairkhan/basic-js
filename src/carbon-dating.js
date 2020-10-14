09707c4310860a649dba3fa3d27c7839d7be93eb
qairkhan <shyngys94@gmail.com>
2020-10-14 15:16:09 +0300

    fix: carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-   let D = parseFloat(sampleActivity),
-   j = (0.693 / HALF_LIFE_PERIOD)
-   if (typeof sampleActivity !== 'string' || Object.is(NaN, D) || D <= 0 || 15 <= N)
-   return false;
-   return Math.ceil(Math.log(MODERN_ACTIVITY / D)/ j)
+  let D = parseFloat(sampleActivity);
+  let J = 0.693 / HALF_LIFE_PERIOD;
+  if(Number.isNaN(D) || (typeof sampleActivity !== 'string') || (D <= 0 || D > 15) ){
+    return false
+  } else {
+    let X = Math.ceil((Math.log(MODERN_ACTIVITY / D)) / J);
+    return X;
+  }
 };
