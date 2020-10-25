4cd1a5d65ef57baa23c5d9e1ce67727d32d57062
Qairkhan <shyngys94@gmail.com>
2020-10-25 22:11:21 +0300

    fix: hanoi tower solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,1 @@
-module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = Math.pow(2, disksNumber) - 1;
-  let seconds = turns / (turnsSpeed / 3600);
-    return { turns, seconds };
-};
+
