e518e18bc6040e859abbea6aaf927236f57808dc
Qairkhan <shyngys94@gmail.com>
2020-10-25 22:35:33 +0300

    feat: add extended-reapeater solition

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,1 +1,39 @@
+module.exports = function repeater(str, options) {
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
 
+  if (!options.separator) {
+    options.separator = "+";
+  }
+
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+
+  let result = "";
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str;
+
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      result += options.addition;
+
+      if (j < options.additionRepeatTimes - 1) {
+        result += options.additionSeparator;
+      }
+    }
+
+    if (i < options.repeatTimes - 1) {
+      result += options.separator;
+    }
+  }
+  return result;
+};
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,31 @@
 module.exports = function transform(arr) {
-  if (!Array.isArray(arr)) throw "Error";
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
 
-  var result = [];
+  const result = [];
 
-  for (var i = 0; i < arr.length; i += 1) {
+  for (let i = 0; i < arr.length; i++) {
     switch (arr[i]) {
       case "--discard-next":
-        i += 1;
+        i++;
         break;
       case "--discard-prev":
-        if (result.length != 0) result.pop();
+        result.pop();
         break;
       case "--double-next":
-        if (i < arr.length - 1) result.push(arr[i + 1]);
+        if (i < arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
         break;
       case "--double-prev":
-        if (i >= 1) result.push(arr[i - 1]);
+        if (i > 0) {
+          result.push(arr[i - 1]);
+        }
         break;
       default:
         result.push(arr[i]);
+        break;
     }
   }
 
